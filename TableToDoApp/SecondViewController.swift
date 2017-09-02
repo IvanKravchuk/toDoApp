faf2056d9773c3e28d114e41beb3aa7dd0654c73
Admin <admin@Mac-Admin.local>
2017-09-02 13:43:08 +0300

    Initial Commit

---
+++ TableToDoApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TableToDoApp
+//
+//  Created by Admin on 02.09.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
